1f6f9222faa63f46320a0a5da556338655b89284
Eric Zarcone <ezarco38@mbp-0179.ad.avant.com>
2018-02-01 17:05:34 -0600

    lets try this again

--- example.tf
+++ example.tf
@@ -1,1 +1,12 @@
-test line one.
+
+export AWS_ACCESS_KEY_ID=AKIAII4RKUUJ4Q5US72A
+export AWS_SECRET_ACCESS_KEY=X1uuhrREJNgWqECGVlzoeHeRsOFXymWUZvWy4DlD
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-2d39803a"
+  instance_type = "t2.micro"
+}
